bbedef2ddf8448949618ffad5f5e0d1b7446d9e4
Peter <p.sivak91@gmail.com>
2021-09-07 23:29:55 -0700

    hope my webpage

---
+++ app.js
@@ -0,0 +1,127 @@
+
+function myFunction() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+ /*  SHOW MODAL AFRER MOUSEOVER */
+
+ const panels = document.querySelectorAll('.paneldresu')
+ const modal = document.querySelector('.modal')
+ const closeModal = document.querySelectorAll('.closebtn')
+ /* const logo = document.querySelectorAll('.logoteamu') */
+
+  panels.forEach(function(panel) {
+    //console.log(panel)
+    const logo = panel.querySelector('.logoteamu')
+    //console.log(logo)
+      logo.addEventListener('click', function(e) {
+        const mod = e.currentTarget.nextSibling.nextElementSibling
+        
+        mod.classList.add('showit')
+        
+
+      /*   panels.forEach(function(item) {
+         
+
+         if(item !== panel){
+           item.classList.remove('showit')
+         }
+          
+        }) */
+        
+      })
+  }) 
+
+  /*  CLOSE MODAL */
+  closeModal.forEach(function(done) {
+    done.addEventListener('click', function(e) {
+      const modal = done.parentElement
+      modal.classList.remove('showit')
+
+      
+    })
+  })
+
+ 
+
+
+//      contact  section ---------------
+ 
+
+let message = []
+
+const addMessage = (e) => {
+  e.preventDefault()
+
+  let msg = {
+    id: Date.now(),
+    firstName: document.getElementById('name').value,
+    lastName: document.getElementById('email').value,
+    text: document.querySelector('textArea').value
+  }
+
+   //name validate
+  const name = document.getElementById('name');
+  const re1 = /^[a-zA-Z]{2,10}$/
+
+   // email validate
+   const email = document.getElementById('email')
+   const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+   
+   if(!re1.test(name.value) || !re.test(email.value)) {
+
+    // name
+    name.classList.add('is-invalid')
+    setTimeout(function(){
+      name.classList.remove('is-invalid');
+    },3000)
+    
+    // email
+    email.classList.add('is-invalid')
+     setTimeout(function(){
+      email.classList.remove('is-invalid');
+    },3000)
+
+    email.value = ''
+    name.value = ''
+
+  } else {
+   
+        message.push(msg)
+       
+        localStorage.setItem('Messagese', JSON.stringify(message))
+        console.warn('added', {message})
+        clearInputs()
+        localStorage.clear();
+   
+        const success = document.querySelector('.success')
+        success.classList.add('show')
+
+        setTimeout(function(){
+          success.classList.remove('show')
+        },2000)
+
+  } 
+
+        
+
+}
+
+
+const clearInputs = () => {
+    document.getElementById('name').value = '',
+    document.getElementById('email').value = '',
+    document.querySelector('textArea').value = ''
+  }
+
+
+
+
+  
+
---
+++ prevCar.js
@@ -0,0 +1,466 @@
+
+
+// data arr obj stats
+const datastats = [
+  {
+    id: 1,
+    data: {
+      club: "Orlando Solar Bears",
+      year: "2014 / 2015",
+      country: 'USA / Florida',
+      league: 'ECHL',
+      seassons : 5,
+      games: 50,
+      goals:5,
+      asst:5,
+      points: 55
+    }
+  },
+  {
+    id: 3,
+    data: {
+      club: "Alaska Aces",
+    year: "13 / 15 / 16",
+    country: 'USA / Alaska',
+    league: 'ECHL',
+    seassons : 3,
+    games: 171,
+    goals:86,
+    asst:182,
+    points: 268
+    }
+  },
+  {
+    id: 7,
+   data: {
+    club: "Fort Wayne Komets",
+    year: "2015 / 2016",
+    country: 'USA / Indiana',
+    league: 'ECHL',
+    seassons : 1,
+    games: 10,
+    goals:2,
+    asst:6,
+    points:8 
+   }
+  },
+  {
+    id: 12,
+   data: {
+    club: "Rapid City Rush",
+    year: "2017 / 2018",
+    country: 'USA / South Dakota',
+    league: 'ECHL',
+    seassons : 1,
+    games: 58,
+    goals:26,
+    asst:34,
+    points: 60
+   }
+  },
+  {
+    id: 5,
+    data: 
+    {
+      club: "Bridgeport Sound Tigers",
+      year: "2014 / 2015",
+      country: 'USA / Connecticut',
+      league: 'AHL',
+      seassons : 1,
+      games: 6,
+      goals:1,
+      asst:0,
+      points: 6
+    }
+  },
+  {
+    id: 13,
+    data : {
+      club: "San Diego Gulls",
+      year: "2018/ 2019",
+      country: 'USA / California',
+      league: 'ECHL',
+      seassons : 1,
+      games: 1,
+      goals: 0,
+      asst: 0,
+      points: 0
+    }
+  },
+  {
+    id: 14,
+    data : {
+      club: "San Francisco Bulls",
+    year: "2012 / 2013",
+    country: 'USA / California',
+    league: 'ECHL',
+    seassons : 1,
+    games: 64,
+    goals: 33,
+    asst: 23,
+    points: 56
+    }
+  },
+  {
+    id: 16,
+    data : {
+      club: "Stockton Thunders",
+    year: "2014 / 2015",
+    country: 'USA / California',
+    league: 'ECHL',
+    seassons : 1,
+    games: 29,
+    goals:10,
+    asst:16,
+    points: 26
+    }
+  },
+  {
+    id: 17,
+    data : {
+      club: "Dukla Trencin",
+      year: "2001 / 2007",
+      country: 'EU / Slovakia',
+      league: 'Slovakia',
+      seassons : 6,
+      games: 254,
+      goals: 39,
+      asst: 33,
+      points: 72
+    }
+  },
+  {
+    id: 18,
+     data : {
+      club: "Tulsa Oilers",
+      year: "2018 / 2019",
+      country: 'USA / Oklahoma',
+      league: 'ECHL',
+      seassons : 1,
+      games: 10,
+      goals:5,
+      asst:3,
+      points: 8
+     }
+  },
+  {
+    id: 19,
+      data : {
+        club: "Utah Grizzlies",
+    year: "2017 / 2018",
+    country: 'USA / Utah',
+    league: 'ECHL',
+    seassons : 1,
+    games: 4,
+    goals:0,
+    asst:1,
+    points: 1
+      }
+  },
+  {
+    id: 15 ,
+     data : {
+      club: "Worcester Sharks",
+      year: "2012 / 2013",
+      country: 'USA / Massachusetts',
+      league: 'AHL',
+      seassons : 1,
+      games: 9,
+      goals:4,
+      asst:1,
+      points: 5
+     }
+  },
+  {
+    id: 13,
+      data : {
+        club: "HK Dubnica",
+    year: "2004 / 2005",
+    country: 'EU / Slovakia',
+    league: 'Slavakia',
+    seassons : 2,
+    games: 34,
+    goals:6,
+    asst:4,
+    points: 10
+      }
+  },
+  {
+    id: 14,
+     data : {
+      club: "Orlando Solar Bears",
+      year: "10/20",
+      country: 'USA / Florida',
+      league: 'ECHL',
+      seassons : 5,
+      games: 50,
+      goals:5,
+      asst:5,
+      points: 55
+     }
+  },
+  {
+    id: 15,
+     data : {
+      club: "Orlando Solar Bears",
+      year: "14/15",
+      country: 'USA / Florida',
+      league: 'ECHL',
+      seassons : 1,
+      games: 25,
+      goals:5,
+      asst:15,
+      points: 20
+     }
+  },
+  {
+    id: 16,
+    data : {
+      club: "Orlando Solar Bears",
+      year: "10/20",
+      country: 'USA / Florida',
+      league: 'ECHL',
+      seassons : 5,
+      games: 50,
+      goals:5,
+      asst:5,
+      points: 55,
+      eu: 12,
+      usa: 8,
+      total: 20,
+      champ: 2
+    } 
+  }
+] 
+
+// data arr obj jersy
+  const jersy = [
+   {
+     imagejersy: "../logos/dress.png",
+     imagelogo: "../logos/ORLANDO_SOLAR_BEARS.png",
+     id:1,
+     name: "orlando"
+   },
+   {
+    imagejersy: "../logos/dress.png",
+    imagelogo: "../logos/ak.png",
+    id:2,
+    name: "alaska"
+  },
+   {
+    imagejersy: "../logos/dress.png",
+    imagelogo: "../logos/komets.png",
+    id:3,
+    name: "komets"
+  },
+  {
+   imagejersy: "../logos/dress.png",
+   imagelogo: "../logos/rapid.png",
+   id:4,
+   name: "rapid"
+ },
+ {
+  imagejersy: "../logos/dress.png",
+  imagelogo: "../logos/sd.png",
+  id:5,
+  name: "sandiego"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/sf.png",
+ id:6,
+ name: "sf"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/stockton.png",
+ id:7,
+ name: "stockton"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/tn.png",
+ id:8,
+ name : "trencin"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/tulsa.png",
+ id:9,
+ name: "tulsa"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/utah.png",
+ id:10,
+ name: "utah"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/shark.png",
+ id:11,
+ name: "sharks"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/za.png",
+ id:12,
+ name: "za"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/psg-zlin.png",
+ id:13,
+ name: "zlin"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/abb.png",
+ id:14,
+ name: "abbot"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/HKM logo_classic_PNG.png",
+ id:15,
+ name: "zvolen"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/brifge.png",
+ id:16,
+ name: "bridgeport"
+}
+,
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/dubnica.png",
+ id:17,
+ name: "dubnica"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/lm.png",
+ id:18,
+ name: "lm"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/povazska.png",
+ id:19,
+ name : "povazska"
+},
+{
+ imagejersy: "../logos/dress.png",
+ imagelogo: "../logos/pn.png",
+ id:20,
+ name: "pn"
+}
+ ]  
+
+
+
+
+ 
+ 
+ // box dresov
+ const panel = document.querySelector('.panel')
+ const modal = document.querySelector('.modal')
+ const logoteamu = document.querySelectorAll('.logoteamu')
+  //console.log(logoteamu)
+
+ window.addEventListener('DOMContentLoaded', function() {
+    logoteamu.forEach(function(logo) {
+     //  console.log(logo)
+      logo.addEventListener('click', function(e) {
+
+         //current logo
+        const curentlogo = e.currentTarget.dataset.id;
+          console.log(curentlogo)
+          
+          //filter select id of data
+          const dataid = datastats.map(function (item){
+            //console.log(item.id)
+            
+            if(item.id === curentlogo) {
+               return item
+              }
+            
+           
+          })
+           
+          
+          /* show moda */
+          if(dataid){
+          modal.classList.add('showit')
+         } else {
+          modal.classList.remove('showit')
+         } 
+
+           callstats() 
+          
+       })
+    })
+})
+ 
+   let showdata = datastats.map(function(item) {
+    return item.id
+  })
+   console.log(showdata)  
+
+    /* call data stats */
+     function callstats (){
+   let displaydata = datastats.map(function(item) {
+     return `
+     
+        <div class="textbox">
+          <h3>Team: ${item.data.club}</h3>
+          <h3>Year: ${item.data.year}</h3>
+          <h3>Seassons: ${item.data.seassons}</h3>
+          <h3>Games: ${item.data.games}</h3>
+          <h3>Leaguee: ${item.data.league}</h3>
+          <h3>Country: ${item.data.country}</h3>
+          <h3>Goals: ${item.data.goals}</h3>
+          <h3>Assists: ${item.data.asst}</h3>
+          <h3>Points: ${item.data.points}</h3>
+          <h3>Awards: ${item.data.awards}</h3>
+          <h3>Links: ${item.data.links}</h3>
+        
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa optio cum voluptates reprehenderit accusantium necessitatibus ipsum rem natus! Consequuntur, eius.</p>
+
+        </div>
+
+        <div class="pictbox"></div>
+   
+     `
+   })
+  
+   displaydata = displaydata.join('')
+   modal.innerHTML = displaydata
+   console.log(displaydata)
+  } 
+ 
+
+   // window even for load data  
+ window.addEventListener('DOMContentLoaded', function(e) {
+    e.preventDefault()
+
+   // show jersy 
+   showingJerseys(jersy)
+}) 
+
+ // function to load jerseys 
+ const showingJerseys = function(jersy){
+  let displayjersy = jersy.map(function(item) {
+    return `
+    <div class="logo" > 
+                 <img src="${item.imagejersy}" class="dress" alt="dress">
+                 <img src="${item.imagelogo}"  class="logoteams" data-id="${item.id}" alt="orlando">
+            </div>
+    `
+  })
+  //console.log(displayjersy) unformated
+   displayjersy = displayjersy.join('')
+  //console.log(displayjersy) formated
+   boxdresov.innerHTML = displayjersy
+} 
